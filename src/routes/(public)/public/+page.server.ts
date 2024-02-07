c1ef5d530e36c5a42fbd14e5db5ac6f5736f6ff2
Tobias <tobias@sernhede.com>
2024-02-07 16:48:52 +0100

    trying something different

--- src/routes/(public)/public/+page.server.ts
+++ src/routes/(public)/public/+page.server.ts
@@ -1,10 +1,9 @@
 import type { PageServerLoad } from "./$types"
-
+import { PUBLIC_HOST } from "$env/static/public"
 export const load: PageServerLoad = async (event) => {
     event.cookies.set("root_test_cookie", "test", {
         path: "/",
         sameSite: 'lax',
-        domain: '.localhost',
         maxAge: 60 * 10,
     })
     return {}
