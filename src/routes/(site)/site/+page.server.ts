cac820c910e19b2747a65acda0e34db1f08cf744
Tobias <tobias@sernhede.com>
2024-02-07 17:01:59 +0100

    Update +page.server.ts

--- src/routes/(site)/site/+page.server.ts
+++ src/routes/(site)/site/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from "./$types"
 import { PUBLIC_HOST } from "$env/static/public"
 export const load: PageServerLoad = async (event) => {
     event.cookies.set("root_test_cookie", "test", {
-        path: ".",
+        path: "/",
         domain: '.platform.sernhe.dev',
         sameSite: 'lax',
         maxAge: 1000 * 60 * 60,
