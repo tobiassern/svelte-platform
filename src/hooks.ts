803de388aa328da8d1ef58c5813b51d3ba33bf68
Tobias <tobias@sernhede.com>
2024-02-07 16:52:46 +0100

    bugfix

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,7 +2,7 @@ import type { Reroute } from '@sveltejs/kit';
 import { PUBLIC_HOST } from '$env/static/public';
 
 export const reroute: Reroute = ({ url }) => {
-    if (url.hostname === PUBLIC_HOST) {
+    if (url.host === PUBLIC_HOST) {
         return '/site' + url.pathname;
     } else if (`app.${PUBLIC_HOST}` === url.host) {
         return '/app' + url.pathname;
