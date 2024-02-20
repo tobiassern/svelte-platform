9f1ab3ef0a671926212b30659aff9bd7affa888f
Tobias <tobias@sernhede.com>
2024-02-20 22:25:53 +0100

    Update hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,10 +4,13 @@ import { PUBLIC_HOST } from '$env/static/public';
 export const reroute: Reroute = ({ url }) => {
 	console.log("## REROUTE ## ", url.host)
 	if (url.host === PUBLIC_HOST) {
+		console.log("## REROUTE : (SITE)")
 		return '/site' + url.pathname;
 	} else if (`app.${PUBLIC_HOST}` === url.host) {
+		console.log("## REROUTE : (APP)")
 		return '/app' + url.pathname;
 	} else {
+		console.log("## REROUTE : (PUBLIC)")
 		return '/public' + url.pathname;
 	}
 };
