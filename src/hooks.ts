260816069800a353cf7dcffe4521eccfd3c56383
Tobias <tobias@sernhede.com>
2024-02-20 22:21:35 +0100

    updates

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,12 +2,11 @@ import type { Reroute } from '@sveltejs/kit';
 import { PUBLIC_HOST } from '$env/static/public';
 
 export const reroute: Reroute = ({ url }) => {
+	console.log("## REROUTE ## ", url.host)
 	if (url.host === PUBLIC_HOST) {
 		return '/site' + url.pathname;
 	} else if (`app.${PUBLIC_HOST}` === url.host) {
 		return '/app' + url.pathname;
-	} else if (`admin.${PUBLIC_HOST}` === url.host) {
-		return '/admin' + url.pathname;
 	} else {
 		return '/public' + url.pathname;
 	}
